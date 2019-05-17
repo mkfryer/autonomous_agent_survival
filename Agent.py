7846994d8ff5b1ae8127fd0a3d8682d77e6d94e7
clange1 <cristinalange10@gmail.com>
2019-05-17 11:34:13 -0600

    final changes on my end

--- Agent.py
+++ Agent.py
@@ -14,25 +14,48 @@ class Agent():
     three water wells to choose to draw from
 
     variables:
-    dist_params - ndarray (3 x 1) - the parameters of the tri-noulli distribution
+    dist_params - ndarray ((3,) array) - the parameters of the tri-noulli distribution
+    location (x,y): randomly generates a location for the agent
+    health (float): Initializes health to 1.
+    self.correct_well (int): 0,1, or 2
+
     """
 
-    def __init__(self, dist_params, change_location = False):
-        #prior distribution parameters
-        self.location = np.array([0.,0.])
+    def __init__(self, confidence = -1):
+        """
+        Parameter:
+        """
+        #initialize random prior distribution parameters
+        dist_params = np.random.random(3)
+        dist_params /= np.sum(dist_params)
         self.dist_params = dist_params
-        if change_location:
-            domain = np.linspace(-1.,1.,101)
-            self.location = np.array([np.random.choice(domain),np.random.choice(domain)])
 
+        #get location of the agent
+        domain = np.linspace(-1.,1.,101)
+        self.location = np.array([np.random.choice(domain),np.random.choice(domain)])
+
+        #adjust
+        self.health = 3
+        self.global_confidence = True if confidence = -1 else False
+
+        self.confidence = confidence if self.global_confidence else np.random.randint(1,50)/100.
+
+    def seed(self, seed_type, correct_well):
+        if not self.global_confidence:
+            self.confidence = np.random.randint(0,100)/10000.
+
+        if seed == "good":
+            self.create_informed_agent(correct_well)
+        else: self.create_bad_agent(correct_well)
 
 
     def get_MLE(self, observations):
         """
         Paramters:
-        observations - ndarray (n x 3):
+            observations ((n,) ndarray): the wells the other agents chose
 
-        Returns ndarray (n x 3):  most likely parameters given the observations
+        Returns:
+            MLE ((n,) ndarray):  most likely parameters given the observations
         """
         # theta_1 = 3
         # theta_2 = 6
@@ -45,10 +68,10 @@ class Agent():
         # res = minimize(liklihood, x0, method='trust-constr', jac=liklihood_jac,  constraints=[linear_constraint])
 
 
-    def update_dist_params(self, obs, c):
+    def update_dist_params(self, obs):
         """
-        c - float - confidence weight of other agents decisions
-        observations - ndarray (n x 3)
+        c (float): confidence weight of other agents decisions
+        observations ((n,)ndarray): list of the wells the other agents chose
         """
         observations = np.array(obs)
         observed_dist = np.array([Tools.percent_correct(observations,0),
@@ -56,12 +79,82 @@ class Agent():
                                   Tools.percent_correct(observations,2)])
         n = observations.size
         #update prior to posterior
-        self.dist_params = self.dist_params + c * n * observed_dist
+        self.dist_params = self.dist_params + self.confidence * n * observed_dist
         #normalize
         self.dist_params /= sum(self.dist_params)
 
-    def act(self):
+    def utility(self, well_locations):
+        distances = np.linalg.norm(self.location-well_locations, axis = 1)
+        self.dist_param = distances/(2*np.sqrt(2))
+
+    def act(self, correct_well, well_locations):
         """
-            returns highest probable good choice
+        Returns highest probable good choice
+        Enacts Consequences (such as death if health = 0)
+
+        This is where we would code up the utility function as well
+        We can see other peoples locations
+
+        parameters:
+            correct_well (int) =  0,1,or 2 which well has water
+            well_locations np.array() = x and y coordinatess of each well
         """
-        return np.argmax(self.dist_params)
+        #get utility function and update distribution parameters
+        self.utility(well_locations)
+
+        #checks if all three elements are tied
+        if len(set(self.dist_params)) ==1:
+                choice = np.random.array(np.array([0,1,2]))
+        else:   choice = np.argmax(self.dist_params)
+
+        #update health depending on right or wrong choice
+        if choice != correct_well:
+            self.health = self.health - 1
+        else:
+            self.health = 3
+
+        return choice
+
+    def create_bad_agent(self, correct_well):
+        dist_params = np.ones(3)
+        dist_params[correct_well] = 0
+        rand = np.random.random()
+        dist_params[dist_params==1] = np.array([rand, 1.-rand])
+        self.dist_params = dist_params
+
+    def create_informed_agent(self,well_index, low_variance = True):
+        """
+        Creates a probable informed agent. If variance is low then
+        there is a very high chance the agent will have really good info.
+        If variance is high, then there is a roughly 50% chance the agent
+        will have really good information.
+
+        well_index (int) - 0,1,2 representing the correct well
+        """
+
+        a, b = (0, 0)
+        dist_params = np.zeros(3)
+
+        #get variables for beta distrb.
+        #these effect the variance
+        if low_variance:
+            a, b = (5, .5)
+        #well-informed
+        else:
+             a, b = (.5, .5)
+
+        #assign infomred probability to the well distr. index
+        dist_params[well_index] = np.random.beta(a, b, 1)
+        #indicies with corresponding zero entries
+        m, = np.where(dist_params == 0)
+
+        #randomly assign other two proabilities
+        t = np.random.random(2)
+        #make sure they add up to 1 - dist_params[well_index]
+        t = (t/np.sum(dist_params)) * (1-dist_params[well_index])
+        dist_params[m] = t
+
+        #re-normalize to account for floating point arth. error
+        dist_params /= sum(dist_params)
+
+        self.dist_params = dist_params
--- World.py
+++ World.py
@@ -1,15 +1,108 @@
-from AgentFactory import AgentFactory
-from AgentFactory import Agent
+from Agent import Agent
 import numpy as np
+import random
 
-class World(AgentFactory):
 
-    def __init__(self, people=100):
+class World():
+    """
+    Objective of this class is to determine what is the well the water is
+    being pumped to and create agents according to the prior type passed in.
+
+    Parameters:
+
+        correctWell (int): 0, 1, or 2
+        wellA (x,y): Tuple of the x- and y-coordinates
+        wellB (x,y): Tuple of the x- and y-coordinates
+        wellC (x,y): Tuple of the x- and y-coordinates
+        population (int): Keeps track of our population numbers
+    """
+
+    def __init__(self, good=0,bad=0, people=200, global_confidence=-1):
+        """ Initialize the case of Uniform, Unique, Good or Bad
+
+        ratio ((2,), ndarray): [%neutral, %good, %bad]
+        """
+        #initialize data list
+        self.get_data = []
+
+        #establish population and ratio
+        self.population = people
+        self.ratio = np.array([people-good-bad,good,bad])/people
+
+        #establish locations of the wells
         distance = np.linspace(-1,1,101)
-        self.wellA_location = (np.random.choice(distance),np.random.choice(distance))
-        self.wellB_location = (np.random.choice(distance),np.random.choice(distance))
-        self.wellC_location = (np.random.choice(distance),np.random.choice(distance))
+        wellA = (np.random.choice(distance),np.random.choice(distance))
+        wellB = (np.random.choice(distance),np.random.choice(distance))
+        wellC = (np.random.choice(distance),np.random.choice(distance))
+        self.well_locations = np.concatenate(([wellA],[wellB],[wellC]))
 
-        self.correctWell = np.random.choice(np.array(["Well A", "Well B", "Well C"]))
+        #make list of agents
+        self.Agent_list = [Agent(global_confidence) for i in range(self.population)]
 
-        
+
+    def seed_list(self, correctWell):
+        """
+
+        Parameters:
+            correctWell (int): 0,1,2 the correct_well
+        """
+        #make them random and get how many good and bad
+        random.shuffle(self.Agent_list)
+        num_Good, num_Bad = tuple(np.round(self.ratio[1:]*self.population).astype(int))
+
+        if self.ratio[0] != 1:
+            #seed the agents
+            for agent in self.Agent_list[:num_Good]:
+                agent.seed("good",correctWell)
+            for agent in self.Agent_list[num_Good:num_Good+num_Bad]:
+                agent.seed("bad",correctWell)
+
+            random.shuffle(self.Agent_list)
+
+    def each_day(self, cascade =True):
+        correctWell = np.random.choice(np.array([0, 1, 2]))
+
+        #seed list
+        self.seed_list(correctWell)
+
+        #tracking the actions of the agents
+        if self.population == 0:
+            return
+        observations = [self.Agent_list[0].act(correctWell, self.well_locations)]
+        for agent in self.Agent_list[1:]:
+            if cascade:
+                agent.update_dist_params(observations)
+            observations.append(agent.act(correctWell, self.well_locations))
+
+        #update agents/population based on deaths
+        Agent_remove = []
+        for agent in self.Agent_list:
+            if agent.health == 0:
+                Agent_remove.append(agent)
+        for agent in Agent_remove:
+            self.Agent_list.remove(agent)
+        self.population = len(self.Agent_list)
+
+        self.collect_data(observations,correctWell)
+
+        #return correctWell, np.sum(np.array(observations) == correctWell), self.population
+
+
+    def collect_data(self, obs, well):
+        """ This function is to collect what we do each day. We call it at the
+        end of the each_day() function and we pass in observations and the
+        correct well. The goal is to see what we do each day
+
+        Parameters:
+            obs (list): list of the actions of the day
+        """
+        well1 = np.sum(np.array(observations) == 0)
+        well2 = np.sum(np.array(observations) == 1)
+        well3 = np.sum(np.array(observations) == 2)
+
+        percent_correct = np.sum(np.array(observations) == correctWell)/self.population
+
+        data = list(self.get_data)
+        data.append([self.population, percent_correct, (well1,well2,well3)])
+
+        self.get_data = np.array(data)
